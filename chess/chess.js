0339d372b0d3cce9557b7aab1c126cd7460e0d77
Jack Zeiders <zeiders@usc.edu>
2016-10-01 05:22:03 -0700

    Checkers

--- chess/chess.js
+++ chess/chess.js
@@ -13,32 +13,28 @@ module.exports = {
 			move = this.parseMove(command);
 			console.log(move);
 			if (this.verifyMove(move)) {
+				// console.log(game.boardState.isGameOver())
 				var result = game.movePiece(move);
-        var winner = 0;
-				if (result != null) {
-					if (game.boardState.winnerIsWhite)
-						winner = 1;
-					if (game.boardState.winnerIsBlack)
-						winner = 2;
-					if (game.boardState.gameIsDrawn)
-						winner = 3;
-					return {
-						game: game,
-						validMove: true,
-						winner: winner
-					}
-				} else {
-					return {
-						game: game,
-						validMove: false,
-						winner: 0
-					}
+				var winner = 0;
+				if (game.boardState.winnerIsWhite === false)
+					winner = 1;
+        if (game.boardState.winnerIsWhite === true)
+					winner = 2;
+				if (game.boardState.gameIsDrawn)
+					winner = 3;
+				return {
+					game: game,
+					validMove: result === null ? false : true,
+					winner: winner,
+					boardString: this.writeBoard(game)
 				}
+
 			} else {
 				return {
 					game: game,
 					validMove: false,
-					winner: 0
+					winner: 0,
+					boardString: this.writeBoard(game)
 				};
 			}
 		}
@@ -68,6 +64,31 @@ module.exports = {
 		return move;
 	},
 	writeBoard: function(game) {
-		return game.toString();
+		var chars = game.toString().split('');
+		for (var i = 0; i < chars.length; i++) {
+			if ((/[a-z]/).test(chars[i])) {
+				chars[i] = "<b>" + chars[i] + "</b>"
+			}
+		}
+		chars[chars.lastIndexOf('1')] = 'a';
+		chars[chars.lastIndexOf('2')] = 'b';
+		chars[chars.lastIndexOf('3')] = 'c';
+		chars[chars.lastIndexOf('4')] = 'd';
+		chars[chars.lastIndexOf('5')] = 'e';
+		chars[chars.lastIndexOf('6')] = 'f';
+		chars[chars.lastIndexOf('7')] = 'g';
+		chars[chars.lastIndexOf('8')] = 'h';
+		chars[chars.indexOf('1') - 2] = "|<br>";
+		chars[chars.indexOf('2') - 2] = "|<br>";
+		chars[chars.indexOf('3') - 2] = "|<br>";
+		chars[chars.indexOf('4') - 2] = "|<br>";
+		chars[chars.indexOf('5') - 2] = "|<br>";
+		chars[chars.indexOf('6') - 2] = "|<br>";
+		chars[chars.indexOf('7') - 2] = "|<br>";
+		chars[chars.indexOf('8') - 2] = "|<br>";
+		chars[chars.indexOf('-') - 2] = "|<br>";
+		chars.unshift('<font face="courier">');
+		chars.push("|</font>");
+		return chars.toString().replace(/,/g, '');
 	}
 };
--- test.js
+++ test.js
@@ -11,7 +11,22 @@ app.post("/", function(req,res){
   // console.log(game);
   data = chessGame.handleInput(sent,game);
   game = data.game;
-  res.send(data.game.toString());
+  if(data.winner != 0){
+    res.send("GAME OVER ");
+  }
+  res.send(chessGame.writeBoard(game));
+})
+app.post("/moves", function(req,res){
+  var game = chessGame.handleInput("start game", "");
+  var moves = req.body.moves.split(",");
+  console.log(moves);
+  for(var i = 0; i < moves.length; i++){
+    data = chessGame.handleInput(moves[i],game);
+    game = data.game;
+    if(data.winner == 1)
+      break;
+    }
+  res.send(data);
 })
 app.listen(3000, function(){
   console.log("started server")
