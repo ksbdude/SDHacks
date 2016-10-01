8f130ac72de787048d0d5cf26894c51b969690e9
Kevin Burns <kevinburns@Kevins-MacBook-Air-3.local>
2016-10-01 03:55:43 -0700

    insert clever comment here

--- sparkpost.js
+++ sparkpost.js
@@ -1,6 +1,38 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var SparkPost = require('sparkpost');
 var sparky = new SparkPost('174c6d51643ab96c495a68a70a2eb2b4c31561e3');
 var firebase = require("firebase");
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+sparky.inboundDomains.all(function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.body);
+    console.log('Congrats you can use our client library!');
+  }
+});
+
+sparky.transmissions.all(function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.body);
+    console.log('Congrats you can use our SDK!');
+  }
+});
 
 var config = {
     apiKey: "AIzaSyD6Xsy4rDSHLL1x6tYSjwlnky6mGz0WZGk",
@@ -67,4 +99,39 @@ function sendEmail(text){
   }
 });
 }
+
+
+
+
+
+
+var processRelayMessage = function(data) {
+  console.log('Email received from: ', data.msg_from);
+  console.log('Searching for: ', data.content.subject);
+  // We start by using the Giphy API to search for gifs based on the subject
+  
+};
+
+// /**
+//   Defines the endpoint that will accept batches from SparkPost
+// **/
+// app.post('/incoming', function(req, res) {
+//   // SparkPost expects a 200 response, send it before processing data
+//   // If you are storing data, do it before returning a response
+//   res.sendStatus(200);
+//   var batch = req.body;
+//   // A batch could contain up to 10,000 events
+//   for(var i=0; i<batch.length; i++) {
+//     // For this application, we can safely assume the batch will only
+//     // contain relay_message events
+//     processRelayMessage(batch[i].msys.relay_message);
+//   }
+// });
+
+// app.listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+
+
 console.log('Done :)');
